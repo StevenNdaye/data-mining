39f4a0c9806c3c06dbe914b9c18347aa1ef69293
StevenNdaye <stevenndaye@gmail.com>
2014-09-09 16:47:43 -0300

    Added the test coverage and functionnality for finding the nearest neighbor

--- src/main/java/com/app/recommend/Rating.java
+++ src/main/java/com/app/recommend/Rating.java
@@ -1,6 +1,8 @@
 package com.app.recommend;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by steven on 2014/09/02.
@@ -19,5 +21,25 @@ public class Rating {
             }
         }
         return distance;
+    }
+
+    public Map<Double, String> computeNearestNeighbor(String user, Map<String, Object> userRatings) {
+
+        Map<Double, String> neighbors = new HashMap<Double, String>();
+
+        for (String username : userRatings.keySet()) {
+            if(username != user){
+                Map<String, Double> firstRatings = (Map<String, Double>) userRatings.get(username);
+                Map<String, Double> secondRatings = (Map<String, Double>) userRatings.get(user);
+
+                double distance = computeManhatanDistance(firstRatings, secondRatings);
+                neighbors.put(distance, username);
+            }
+        }
+        return sortNeighbors(neighbors);
+    }
+
+    private Map<Double, String> sortNeighbors(Map<Double, String> neighbors) {
+        return new TreeMap<Double, String>(neighbors);
     }
 }
--- src/test/java/com/app/recommend/RatingTest.java
+++ src/test/java/com/app/recommend/RatingTest.java
@@ -3,6 +3,8 @@ package com.app.recommend;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -35,4 +37,25 @@ public class RatingTest {
         assertThat(manhatanDistance, is(new Double(7.5)));
     }
 
+    @Test
+    public void itShouldComputeNearestNeighbor(){
+
+        Map<Double, String> neighbors = new HashMap<Double, String>(){{
+            put(new Double(2.0), "Veronica");
+            put(new Double(4.0), "Chan");
+            put(new Double(4.0), "Sam");
+            put(new Double(4.5), "Dan");
+            put(new Double(5.0), "Angelica");
+            put(new Double(5.5), "Bill");
+            put(new Double(7.5), "Jordyn");
+        }};
+
+        Map<Double, String> data = rating.computeNearestNeighbor("Hailey", factory.createRatings());
+        assertThat(data, is(neighbors));
+        assertThat(getFirstNeighbor(data), is("Veronica"));
+    }
+
+    private String getFirstNeighbor(Map<Double, String> data) {
+        return data.get(data.keySet().toArray()[0]);
+    }
 }
